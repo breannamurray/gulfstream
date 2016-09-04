bc9cc77c02726e1268eb2aa7965d6791cc141604
Breanna Murray <breannamurray@purdue.edu>
2016-09-04 15:30:40 -0500

    commit

---
+++ Desktop/breannamurray.github.io/flow/index.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.dropdown ul li').on('click', function() {
+    var label = $(this).parent().parent().children('label');
+    label.attr('data-value', $(this).attr('data-value'));
+    label.html($(this).html());
+
+    $(this).parent().children('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+    
+    $('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+});
+
